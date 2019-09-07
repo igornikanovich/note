605d6bd3d0e4ea908c01674cb6aa24bab82e1dc7
IgorNikanovich <ignikanovich@gmail.com>
2019-09-08 02:30:08 +0300

    create simple views

--- NoteApp/views.py
+++ NoteApp/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView
+from django.urls import reverse
 
-# Create your views here.
+from .models import Category, Note
+
+
+class CategoryListView(generic.ListView):
+    categoryList = Category.objects.all()
+    template_name = 'index.html'
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ['name', ]
+
+    def get_success_url(self):
+        return reverse('index')
+
+
+class NoteListView(generic.ListView):
+    noteList = Note.objects.all()
+    template_name = 'noteapp/note_list.html'
+
+
+class NoteCreateView(CreateView):
+    model = Note
+    fields = ['title', 'text', ]
+
+    def get_success_url(self):
+        return reverse('note-list')
