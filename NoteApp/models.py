fd1596464ce94d8cbbd02eed0e4023ee49ea30b2
IgorNikanovich <ignikanovich@gmail.com>
2019-09-08 02:12:58 +0300

    create models

--- NoteApp/models.py
+++ NoteApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category-detail', args=[self.slug])
+
+
+class Note(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='notes')
+    title = models.CharField(max_length=20)
+    text = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.title
