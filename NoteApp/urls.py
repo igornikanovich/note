076fc97d59703daa63c1931f232465e5c9cccff9
IgorNikanovich <ignikanovich@gmail.com>
2019-09-14 00:22:33 +0300

    update view for add category on index

--- NoteApp/urls.py
+++ NoteApp/urls.py
@@ -5,10 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.CategoryListView.as_view(), name='index'),
-    path('category/create/', views.CategoryCreateView.as_view(), name='category-create'),
-    path('category/<slug>', views.CategoryDetailView.as_view(), name='category-detail'),
+    # path('category/create/', views.CategoryCreateView.as_view(), name='category-create'),
+    # path('category/<slug>', views.CategoryDetailView.as_view(), name='category-detail'),
     path('category/<slug>/create/', views.add_note_to_notelist, name='note-create'),
-    # path('note/<slug>/update', views.my_view, name='note-update'),
     path('note/<slug>/update', views.NoteTextUpdateView.as_view(), name='note-update'),
     path('note/<slug>', views.NoteDetailView.as_view(), name='note-detail'),
 ]
--- NoteApp/views.py
+++ NoteApp/views.py
@@ -1,8 +1,10 @@
+from sqlite3 import IntegrityError
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView
-from django.urls import reverse
+from django.urls import reverse, NoReverseMatch
 
 from .utils import crypt
 from .models import Category, Note
@@ -14,24 +16,32 @@ class CategoryListView(generic.ListView):
     queryset = Category.objects.all()
     template_name = 'index.html'
 
+    def post(self, request, *args, **kwargs):
+        new_category = request.POST['addnewcat']
+        if len(new_category) > 0:
+            try:
+                category = Category.objects.create(name=new_category)
+                category.save()
+                return redirect('index')
+            except:
+                return redirect('index')
+        else:
+            return redirect('index')
 
-class CategoryCreateView(CreateView):
-    model = Category
-    fields = ['name', ]
-    template_name = 'noteapp/category_form.html'
-
-    def get_success_url(self):
-        return reverse('index')
+# class CategoryCreateView(CreateView):
+#     # model = Category
+#     # fields = ['name', ]
+#     template_name = 'index.html'
 
 
-class CategoryDetailView(generic.DetailView):
-    model = Category
-    template_name = 'noteapp/category_detail.html'
+# class CategoryDetailView(generic.DetailView):
+#     model = Category
+#     template_name = 'noteapp/category_detail.html'
 
 
-class NoteListView(generic.ListView):
-    queryset = Note.objects.all()
-    template_name = 'noteapp/note_list.html'
+# class NoteListView(generic.ListView):
+#     queryset = Note.objects.all()
+#     template_name = 'noteapp/note_list.html'
 
 
 class NoteDetailView(generic.DetailView):
@@ -53,35 +63,22 @@ def add_note_to_notelist(request, slug):
     return render(request, 'noteapp/note_form.html', {'form': form})
 
 
-
 class NoteTextUpdateView(UpdateView):
-    model = Note
-    fields = ['text', ]
-    template_name = 'noteapp/note_form.html'
-
 
     def get(self, request, *args, **kwargs):
-        slug = self.kwargs['slug']
-        note = Note.objects.get(slug=slug)
-        title = note.title
-        text = note.text
-        form = NoteForm(initial={'title': title, 'text': text})
-        context = {'form': form}
-        return render(request, "noteapp/note_form.html", context)
-
+        note = Note.objects.get(slug=self.kwargs['slug'])
+        form = NoteForm(initial={'title': note.title, 'text': note.text})
+        return render(request, "noteapp/note_detail.html", {'form': form})
 
     def post(self, request, *args, **kwargs):
         queryset = Note.objects.all()
         note = get_object_or_404(queryset, slug=kwargs['slug'])
         form = NoteForm(request.POST)
         passw = request.POST['password']
-        print(passw)
         if form.is_valid() and passw == CRYPT_PASSWORD:
             notetext = form.cleaned_data['text']
             notetext = crypt(notetext)
-            print(notetext)
             note.text = notetext
-            # note = form.save()
             note.save()
             return HttpResponseRedirect(reverse('note-update', args=[note.slug]))
-        return render(request, 'noteapp/note_form.html', {'form': form})
+        return render(request, 'noteapp/note_detail.html', {'form': form})
